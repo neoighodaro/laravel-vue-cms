e108615940489ad8884660843b7f38f7f8090cde
Neo Ighodaro <neo@creativitykills.co>
2018-08-30 19:16:20 +0100

    updates

--- resources/assets/js/components/Read.vue
+++ resources/assets/js/components/Read.vue
@@ -1,19 +1,15 @@
-
 <template>
-    
-  <div id="posts">
-   <template>
-    <p class="border p-3" v-for="post in posts"> 
-        {{ post.title }} 
-        <router-link :to="{ name: 'update', params: { postID : post.id } }"><button type="button" class="p-1 mx-3 float-right btn btn-light"> Update </button></router-link> 
-        <button type="button" @click="deletePost(post.id)" class="p-1 mx-3 float-right btn btn-danger"> Delete </button>
-    </p>
-  </template>
-    <div>
-    <button v-if="next" type="button" @click="navigate(next)" class="m-3 btn btn-primary">Next </button>
-    <button v-if="prev" type="button" @click="navigate(prev)" class="m-3 btn btn-primary">Previous </button>
+    <div id="posts">
+        <p class="border p-3" v-for="post in posts">
+            {{ post.title }}
+            <router-link :to="{ name: 'update', params: { postID : post.id } }"><button type="button" class="p-1 mx-3 float-right btn btn-light"> Update </button></router-link>
+            <button type="button" @click="deletePost(post.id)" class="p-1 mx-3 float-right btn btn-danger"> Delete </button>
+        </p>
+        <div>
+            <button v-if="next" type="button" @click="navigate(next)" class="m-3 btn btn-primary">Next </button>
+            <button v-if="prev" type="button" @click="navigate(prev)" class="m-3 btn btn-primary">Previous </button>
+        </div>
     </div>
-</div>
 </template>
 
 
