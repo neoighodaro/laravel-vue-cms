e08d133e3ec9b8417ea7a88e2728aa65219eef1a
Neo Ighodaro <neo@creativitykills.co>
2018-08-30 18:30:44 +0100

    Updates

--- README.md
+++ README.md
@@ -4,13 +4,16 @@ This is an example of how to build a CMS from scratch with Laravel and Vue. You 
 
 ## Getting Started
 
-* Download or clone the project
-* Rename .env.example to .env
-* Configure the database settings in the .env file
-* Update the PUSHER_APP_* keys in the .env file
-* Change the BROADCAST_DRIVER to pusher
-* Run the command: `php artisan serve && npm run dev` to start the app
+-   Download or clone the project.
+-   `cd` to the project directory.
+-   Rename `.env.example` to `.env`
+-   Configure the database settings in the `.env` file.
+-   Update the `PUSHER_APP_*` keys in the `.env` file.
+-   Change the `BROADCAST_DRIVER` to `pusher`.
+-   Run the command `npm install` to install the dependencies.
+-   Run the command: `npm run dev` to start the app.
 
 ## Built With
-* Laravel framework
-* PHP
+
+-   [Laravel](https://laravel.com) framework
+-   PHP
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,35 +1,35 @@
-
 <!DOCTYPE html>
 <html lang="en">
-<head>
 
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <script> window.Laravel = { csrfToken: 'csrf_token() ' } </script>
+    <script>
+        window.Laravel = { csrfToken: 'csrf_token() ' }
+    </script>
     <title> Welcome to the Admin dashboard </title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     <style>
-        html, body {
-        background-color: #202B33;
-        color: #738491;
-        font-family: "Open Sans";
-        font-size: 16px;
-        font-smoothing: antialiased;
-        overflow: hidden;
+        html,
+        body {
+            background-color: #202B33;
+            color: #738491;
+            font-family: "Open Sans";
+            font-size: 16px;
+            font-smoothing: antialiased;
+            overflow: hidden;
         }
     </style>
-    
 </head>
+
 <body>
-
-
-<div id="app">
-<Homepage :user-name="{{ json_encode(Auth::user()->name) }}" :user-i-d="{{ json_encode(Auth::user()->id) }}"></Homepage>
-</div>
-<script src="{{ asset('js/app.js') }}"></script>
+    <div id="app">
+        <Homepage :user-id='@json(Auth::user()->id)' :user-name='@json(Auth::user()->name)'>
+        </Homepage>
+    </div>
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
+
 </html>
