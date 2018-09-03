6b096b23b1987a8f8561dba776f8c304c2678cc7
Neo Ighodaro <neo@creativitykills.co>
2018-09-03 19:51:57 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,9 +10,6 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
@@ -75,5 +72,7 @@
             @yield('content')
         </main>
     </div>
+        <script src="{{ asset('js/app.js') }}"></script>
+
 </body>
 </html>
