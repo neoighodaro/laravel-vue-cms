4b50f353592a11d7150f56c2a36a4942f80c72e3
Neo Ighodaro <neo@creativitykills.co>
2018-09-03 19:50:55 +0100

    Update master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,7 +8,6 @@
     <meta name="author" content="Neo Ighodaro">
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <script src="{{ asset('js/app.js') }}" defer></script>
     <title>LaravelCMS</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     
@@ -54,5 +53,7 @@
         <p class="m-0 text-center text-white">Copyright &copy; LaravelCMS 2018</p>
       </div>
     </footer>
+        <script src="{{ asset('js/app.js') }}"></script>
+
   </body>
 </html>
